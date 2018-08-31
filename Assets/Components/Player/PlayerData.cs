988f5bd36db856669544dc946cdca3ad7a628438
Jason Bandy <jbandy98@gmail.com>
2018-08-30 19:50:45 -0500

    Fixed networking bugs and deployed new windows build

--- Assets/Components/Player/PlayerData.cs
+++ Assets/Components/Player/PlayerData.cs
@@ -10,4 +10,5 @@ public class PlayerData {
     public string zoneName;
     public string sessionId;
 
+
 }
--- Assets/View/PlayerManager.cs
+++ Assets/View/PlayerManager.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Photon;
 
-public class PlayerManager : MonoBehaviour {
-
+[RequireComponent(typeof(PhotonView))]
+public class PlayerManager : Photon.MonoBehaviour, IPunObservable {
+    [SerializeField]
     public PlayerData data;
+
     public TextMeshPro playerText;
 
-	// Use this for initialization
-	void Start () {
+    void Start()
+    {
         playerText.text = data.playerName;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(data.playerName);
+        }
+        else
+        {
+            data.playerName = (string)stream.ReceiveNext();           
+        }
+        playerText.text = data.playerName;
+    }
 }
