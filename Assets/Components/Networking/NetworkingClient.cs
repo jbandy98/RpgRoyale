2227a99c913230419029e948a72238c329d9140c
Jason Bandy <jbandy98@gmail.com>
2019-07-02 20:29:02 -0500

    More work on combat features and websocket networking

---
+++ Assets/Components/Networking/NetworkingClient.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System;
+using System.Net;
+using System.IO;
+using UnityEngine.UI;
+
+public class NetworkingClient : MonoBehaviour
+{
+    public string host = "localhost/combat/connect";
+    public int port = 7112;
+    private bool socketReady = false;
+    private TcpClient socket;
+    private NetworkStream stream;
+    private StreamReader reader;
+    private StreamWriter writer;
+    public Text hostField;
+    public Text portField;
+
+    void Start()
+    {
+        hostField.text = host;
+        portField.text = port.ToString();
+
+    }
+
+    public void StartConnection()
+    {
+        host = hostField.text;
+        port = Int32.Parse(portField.text);
+        OnConnectedToServer();
+    }
+
+    public void OnConnectedToServer()
+    {
+        // if already connected, ignore this function
+        if (socketReady)
+            return;
+
+        // create the socket
+        try
+        {
+
+            socket = new TcpClient(host, port);
+            stream = socket.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+            socketReady = true;
+            Debug.Log("Client created?");
+
+        } catch (Exception e)
+        {
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    void Update()
+    {
+        if(socketReady)
+        {
+            if(stream.DataAvailable)
+            {
+                string data = reader.ReadLine();
+                if (data != null)
+                {
+                    OnIncomingData(data);
+                }
+            }
+        }
+    }
+
+    private void OnIncomingData(string data)
+    {
+        Debug.Log("Server: " + data);
+    }
+
+
+}
---
+++ Assets/Components/Networking/WebSocketSharpDemo.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using UnityEngine.UI;
+
+public class WebSocketSharpDemo : MonoBehaviour
+{
+    public Text hostField;
+    public Text messageField;
+
+    WebSocket webSocket;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void StartConnection()
+    {
+        webSocket = new WebSocket(hostField.text);
+        webSocket.OnOpen += OnOpenHandler;
+        webSocket.OnMessage += OnMessageHandler;
+        webSocket.OnClose += OnCloseHandler;
+
+        webSocket.ConnectAsync();
+    }
+
+    public void SendMessage()
+    {
+        webSocket.Send(messageField.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnOpenHandler(object sender, System.EventArgs e)
+    {
+        Debug.Log("Websocket connected!");
+    }
+
+    private void OnSendComplete(bool success)
+    {
+        Debug.Log("Message sent successfully? " + success);
+    }
+
+    private void OnMessageHandler(object sender, MessageEventArgs e)
+    {
+        Debug.Log("Websocket server said: " + e.Data);
+    }
+
+    private void OnCloseHandler(object sender, CloseEventArgs e)
+    {
+        Debug.Log("Websocket closed with reason: " + e.Reason);
+    }
+}
